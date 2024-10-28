305ea457ec14b965a072ba5f22d706613f5d69b5
Daniel Rosendorf <daniel.rosendorf@googlemail.com>
2024-10-28 20:24:49 +0100

    Add shoelace assets

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import { copy } from "@web/rollup-plugin-copy";
+import rcopy from "rollup-plugin-copy";
 
 export default {
   input: "index.html",
@@ -9,5 +10,14 @@ export default {
     html(),
     nodeResolve(),
     copy({ patterns: "./assets/**/*.{svg,jpg,json,png}" }),
+    rcopy({
+      copyOnce: true,
+      targets: [
+        {
+          src: "./node_modules/@shoelace-style/shoelace/dist/assets",
+          dest: "./dist",
+        },
+      ],
+    }),
   ],
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 // shoelace imports via cherry-picking
 import SlButton from "@shoelace-style/shoelace/dist/components/button/button.js";
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
-setBasePath("./node_modules/@shoelace-style/shoelace/dist/");
+setBasePath("./dist");
 
 export default {};
