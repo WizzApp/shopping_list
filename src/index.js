96e3fa05909d37ea4ba2e75b42252c1a8f145993
Daniel Rosendorf <daniel.rosendorf@googlemail.com>
2024-10-28 20:31:06 +0100

    Copy Shoelace assets, remove copy on build

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
 // shoelace imports via cherry-picking
 import SlButton from "@shoelace-style/shoelace/dist/components/button/button.js";
-import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
-setBasePath("./dist");
 
 export default {};
