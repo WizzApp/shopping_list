ebcf3114ede871f6f6c9f2dad831a8d37a11e699
Daniel Rosendorf <daniel.rosendorf@googlemail.com>
2024-10-28 19:27:22 +0100

    Add rollupt-plugin-copy

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,8 +1,13 @@
 import { rollupPluginHTML as html } from "@web/rollup-plugin-html";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { copy } from "@web/rollup-plugin-copy";
 
 export default {
   input: "index.html",
   output: { dir: "dist" },
-  plugins: [html(), nodeResolve()],
+  plugins: [
+    html(),
+    nodeResolve(),
+    copy({ patterns: "./assets/**/*.{svg,jpg,json}" }),
+  ],
 };
