2532dc6eb06642cce4afac7c58b0e8fb7b3999b2
Daniel Rosendorf <daniel.rosendorf@googlemail.com>
2024-10-28 20:34:50 +0100

    Add setBasePath again

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 // shoelace imports via cherry-picking
 import SlButton from "@shoelace-style/shoelace/dist/components/button/button.js";
+import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
+setBasePath("./");
 
 export default {};
